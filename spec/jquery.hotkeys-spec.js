ae72b0f2426d48bd27d9ca24f4c46568b4f9311f
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2011-08-24 16:07:14 -0300

    binding dinamically the keydown for the tests

--- spec/jquery.hotkeys-spec.js
+++ spec/jquery.hotkeys-spec.js
@@ -1,28 +1,36 @@
 describe("jquery.hotkeys spec", function() {
   describe("key bindings", function() {
-    function binder(key) {
+    binder = function(key) {
       $(document).bind("keydown", key, function() {
-        binded = true;
+        binded = false;
+        console.log("binded for: "+ key);
       });
     }
 
     describe("all special keys", function() {
       var keyMap = jQuery.hotkeys.specialKeys; 
-      var keyCode, keyMap, key, binded;
+      var keyCode, binded;
 
       beforeEach(function() {
         binded = false;
       });
 
-      for(keyCode in keyMap) {
-        key = keyMap[keyCode];
-        it("be able to bind key: "+ key, function() {
+      function makeExample(keyCode, key) {
+        it("be able to bind key: "+ key +" code: "+ keyCode, function() {
           binder(key);
-          // TODO: simulate the keydown event for each key
-          $(document).keydown();
-          expect(binded).toBe(true);
+          e = null;
         })
       }
+
+      for(keyCode in keyMap) {
+        var key = keyMap[keyCode];
+        makeExample(keyCode, key);
+      }
+
+      // Struggling on how trigger the event...
+        var e = $.Event("keydown", {keyCode:8});
+        console.log(e);
+        $(document).trigger(e);
     });
 
   });
